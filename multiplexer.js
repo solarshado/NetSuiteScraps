c95b30f37a44839e13e8261f1dcfb0a41787bb87
Adrian Todd <solarshado@gmail.com>
2018-11-29 00:40:32 -0500

    broad(ish) refactor into a callable module instead of a copyable template

--- multiplexer.js
+++ multiplexer.js
@@ -1,46 +1,52 @@
 /**
  * @NApiVersion 2.x
- * @NScriptType ClientScript
  * @NModuleScope SameAccount
  */
-define(["myCustomScriptOne","myCustomScriptTwo"], function(delegateOne,delegateTwo) {
-	// myCustomScriptOne and myCustomScriptTwo are normal client scripts
+define([], function() {
+	/**
+	 * Builds a multiplexer that can be returned to NS for the given set of
+	 * events using the given delegates.
+	 *
+	 * @param eventDescriptors an array of {name, hasReturn} objects describing the events to delegate
+	 * @param delegates an array of other event-handling modules to delegate to
+	 *
+	 * @returns an object returnable from a module that can act as an event handler
+	 */
+	function buildMultiplexer(eventDescriptors, delegates) {
+		// Note: returns undef if no delegates appear to be interested in the named event
+		function makeDelegator(eventName, hasReturn) {
+			var delegateHandlers = [];
 
-	//var delegates = Array.from(arguments);
-	// if Array.from isn't supported:
-	//var delegates = Array.prototype.slice.call(arguments);
-	// unfortunate last resort:
-	//var delegates = [delegateOne,delegateTwo];
+			for(var i = 0; i < delegates.length; ++i)
+				if(eventName in delegates[i] && typeof delegates[i][eventName] === "function")
+					delegateHandlers.push(delegates[i][eventName]);
 
-	// this should work...
-	var delegates = (Array.from || Array.prototype.slice.call)(arguments);
+			return delegateHandlers.length === 0 ? undefined :
+				hasReturn ?
+				function delegatorWithoutReturn(param) {
+					for(var i = 0; i < delegateHandlers.length; ++i)
+						delegateHandlers[i](param);
+				} :
+				function delegatorWithReturn(param) {
+					for(var i = 0; i < delegateHandlers.length; ++i)
+						if(!delegateHandlers[i](param))
+							return false;
+						// else continue looping
+					// all delegates returned true, so
+					return true;
+				};
+		}
 
-	// Note: returns undef if no delegates appear to be interested in the named event
-	function makeDelegator(eventName, hasReturn) {
-		var delegateHandlers = [];
-
-		for(var i = 0; i < delegates.length; ++i)
-			if(eventName in delegates[i] && typeof delegates[i][eventName] === "function")
-				delegateHandlers.push(delegates[i][eventName]);
-
-		return delegateHandlers.length === 0 ? undefined :
-			hasReturn ?
-			function delegatorWithoutReturn(param) {
-				for(var i = 0; i < delegateHandlers.length; ++i)
-					delegateHandlers[i](param);
-			} :
-			function delegatorWithReturn(param) {
-				for(var i = 0; i < delegateHandlers.length; ++i)
-					if(!delegateHandlers[i](param))
-						return false;
-					// else continue looping
-				// all delegates returned true, so
-				return true;
-			};
+		var retVal = {};
+		for(var i = 0; i < events.length; ++i) {
+			var handler = makeDelegator(events[i].name, events[i].hasReturn);
+			if(handler) // don't add undefs, NS might choke on them
+				retVal[events[i].name] = handler;
+		}
+		return retVal;
 	}
 
-	// this may make it easier to generalize this code for other script types
-	var events = [
+	var CLIENT_EVENTS = [
 		{name: "pageInit", hasReturn: false},
 		{name: "fieldChanged", hasReturn: false},
 		{name: "postSourcing", hasReturn: false},
@@ -53,11 +59,13 @@ define(["myCustomScriptOne","myCustomScriptTwo"], function(delegateOne,delegateT
 		{name: "saveRecord", hasReturn: true},
 	];
 
-	var retVal = {};
-	for(var i = 0; i < events.length; ++i) {
-		var handler = makeDelegator(events[i].name, events[i].hasReturn);
-		if(handler) // don't add undefs, NS might choke on them
-			retVal[events[i].name] = handler;
-	}
-	return retVal;
+	// TODO get descriptor lists for other script types' events
+
+	return {
+		"buildMultiplexer": buildMultiplexer,
+		"EVENTS": {
+			"CLIENT": CLIENT_EVENTS,
+			// TODO add other descriptor lists here
+		}
+	};
 });
